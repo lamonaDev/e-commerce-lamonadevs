b5af51b148630cdf90e3f9063929dbdfa286000f
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-09-17 18:02:24 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
-const protectedRoutes = ['/home']
-const publicRoutes = ['/login', '/signup', '/']
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+const protectedRoutes = ['/home'];
+const publicRoutes = ['/login', '/signup', '/'];
+
 export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
-  const isPublicRoute = publicRoutes.includes(path)
-  const token = req.cookies.get('token')?.value || window.localStorage.getItem('token') || null
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token')?.value || null; // Removed window.localStorage
   if (isProtectedRoute && !token) {
-    return NextResponse.redirect(new URL('/login', req.nextUrl))
+    return NextResponse.redirect(new URL('/login', req.nextUrl));
   }
-  if (
-    isPublicRoute &&
-    token
-  ) {
-    return NextResponse.redirect(new URL('/home', req.nextUrl))
+  if (isPublicRoute && token) {
+    return NextResponse.redirect(new URL('/home', req.nextUrl));
   }
-  return NextResponse.next()
+  return NextResponse.next();
 }
+
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
+};
