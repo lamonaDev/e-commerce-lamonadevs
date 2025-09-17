14c2a241f96d28eb7814b43245e529fc7d2ec897
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-09-17 20:06:04 +0300

    fix the middleware

--- middleware.ts
+++ middleware.ts
@@ -7,13 +7,15 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
-  const token = req.headers.get('authorization')?.replace('Bearer ', '') || null;
+  // const token = req.headers.get('authorization')?.replace('Bearer', '') || null;
+  const localToken = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+  // Check for token in headers (if passed from client)
 
-  if (isProtectedRoute && !token) {
+  if (isProtectedRoute && !localToken) {
     return NextResponse.redirect(new URL('/login', req.nextUrl));
   }
 
-  if (isPublicRoute && token) {
+  if (isPublicRoute && localToken) {
     return NextResponse.redirect(new URL('/home', req.nextUrl));
   }
 
